4af0dbb735ac016d825e32ba02d25990a417f75d
Michael Bestas <mkbestas@lineageos.org>
2023-02-28 17:46:30 +0200

    Drop TWRP makefile
    Hasn't been supported in years
    
    Change-Id: I4e8b4fb4c7fa3d8fc369170c0f4e777ffa12d8d2

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -12,8 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TWRP
--include vendor/lineagetwrp/BoardConfigExtra.mk
-
 # Private vendor
 -include vendor/mikeioannina/BoardConfigExtra.mk
